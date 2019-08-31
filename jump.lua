3ab3b6c4cd4866c015517dbd3b2967a96a5ebc53
jumpsplat120 <jumpsplat120@yahoo.com>
2019-08-30 20:59:36 -0700

    Better centering

--- jump.lua
+++ jump.lua
@@ -6,15 +6,17 @@ local splash = {}
 local runtime, length, video, win
 
 function splash.load()
-	win = {}
+	win   = {}
+	video = {}
 	
 	runtime = 0
 	length  = 7
 	
 	win.w, win.h, win.flags = love.window.getMode()
-	
-	--ORIGINAL VIDEO: 1920x1080
-	video = love.graphics.newVideo(folder .. "/assets/jump/video.ogv")
+
+	video.element = love.graphics.newVideo(folder .. "/assets/jump/video.ogv")
+	video.height  = 1080
+	video.width   = 1920
 	
 	return true
 end
@@ -22,17 +24,18 @@ end
 function splash.update(dt, w, h)
 	win.w = w or win.w
 	win.h = h or win.h
-	if not (video:isPlaying()) then
+	if not (video.element:isPlaying()) then
 		love.graphics.setColor(1,1,1,1)
-		video:play()
+		video.element:play()
 	end
 	runtime = runtime + dt
 	if runtime >= length then return false else return true end
 end
 
 function splash.draw()
-	local scale = win.h / 1080
-	love.graphics.draw(video, 0, 0, 0, scale, scale)
+	local scale = win.w / video.width
+	local y = (math.abs(win.h - (scale * video.height))) * .5
+	love.graphics.draw(video.element, 0, y, 0, scale, scale)
 end
 
 
